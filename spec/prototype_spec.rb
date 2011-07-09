221c3d78d01df949ebc420f9641849aa10c5c9b8
Richard LeBer <richard.leber@gmail.com>
2011-07-09 15:40:47 -0400

    Misc fixes to prototype

--- spec/prototype_spec.rb
+++ spec/prototype_spec.rb
@@ -1,4 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Prototype do
+  it "fails" do
+    fail
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
 require File.join(File.dirname(__FILE__), '../lib/prototype')
+
+def capture(*streams)
+  streams.map! { |stream| stream.to_s }
+  begin
+    result = StringIO.new
+    streams.each { |stream| eval "$#{stream} = result" }
+    yield
+  ensure
+    streams.each { |stream| eval("$#{stream} = #{stream.upcase}") }
+  end
+  result.string
+end
