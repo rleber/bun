d8c036c5d98e6cd07c27bf5989546d9783fd2837
Richard LeBer <richard.leber@gmail.com>
2012-02-15 22:28:26 -0500

    Add some descriptive text about the play description language

--- lib/fass/bot.rb
+++ lib/fass/bot.rb
@@ -7,6 +7,23 @@ class Fass
     
     SCRIPTS_DIRECTORY = 'scripts'
     
+    desc "describe", "Describe the scripting language"
+    def describe
+      puts <<-END
+The script engine provides a simple text description language for entering and displaying scripts for
+plays. The syntax for this language is based on ERB and Ember. See http://snk.tuxfamily.org/lib/ember
+for additional details on the syntax.
+  
+The specific directives which this tool understands include:
+character
+characters
+scene
+direction
+song
+rev
+      END
+    end
+    
     desc 'render FILE', "Render a script file"
     def render(file)
       file = "#{SCRIPTS_DIRECTORY}/#{file}" unless file =~ %r{^/}
