14e53c5e0fd813b06f556736712cfea5ff4efd3e
Richard LeBer <richard.leber@gmail.com>
2012-02-18 05:03:02 -0500

    Fine tune tools, clean up 1980 script

--- lib/fass/bot.rb
+++ lib/fass/bot.rb
@@ -51,6 +51,8 @@ class Fass
     
     SCENE_END = /-\s*\bf\s*i\s*n\b\s*-/
     CAST_OF_CHARACTERS = /cast\s+of\s+characters\s*:\s*$/i
+    CAST_LIST = /^(\s*([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\))+\s*$/i
+    CAST_MEMBER = /([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\)/i
     
     desc "clean1 FILE", "Clean an extracted text file (first pass)"
     def clean1(file)
@@ -112,8 +114,11 @@ class Fass
           new_content << "#CAST FOR SCENE:"
           line_index += 1
           cast = []
-          while line_index < cast_starts[scene_index+1] && old_content[line_index] =~ /^\s*([a-z\s]+?)\s+\(\s*([a-z]+)\)$/i
-            cast << [$2, $1]
+          while line_index < cast_starts[scene_index+1] && (old_content[line_index] =~ CAST_LIST || old_content =~ /^\s*$/)
+            new_cast = old_content[line_index].scan(CAST_MEMBER)
+            new_cast.each do |full, nick|
+              cast << [nick.strip, full.strip]
+            end
             line_index += 1
           end
           if cast.size > 0
@@ -139,6 +144,7 @@ class Fass
       warn "Please mark the end of each song with \"END OF SONG\" line."
     end
     
+    START_OF_SONG = /^SONG:/
     END_OF_SONG = /\bEND\s+OF\s+SONG\b/
     desc "clean2 FILE", "Clean an extracted text file (Second pass)"
     def clean2(file)
@@ -150,8 +156,13 @@ class Fass
       content.each do |line|
         if line=~/\*{5}/ .. line=~END_OF_SONG
           new_content << "SONG:" unless in_song
-          new_content << "    #{line}" unless line =~ END_OF_SONG
-          in_song = true
+          if line =~ END_OF_SONG
+            new_content << line
+            in_song = false
+          else
+            new_content << "    #{line}"
+            in_song = true
+          end
         else
           if in_song
             new_content << "END OF SONG"
@@ -160,28 +171,32 @@ class Fass
           new_content << line
         end
       end
-      
+     
       # Now, fix line endings and merge lines
+      # TODO Better handling of hypenated words?
       old_content = new_content
       new_content = []
       preserve_breaks = false
+      last_line = ""
       old_content.each do |line|
         new_content << "" if preserve_breaks ||
+                             last_line =~ END_OF_SONG || 
                              new_content.size == 0 ||
                              line =~ /^.[LS]-\d+(?:\]|$|.\s*\()/ ||
                              line =~ /^\([A-Z]/ ||
-                             line =~ /^[A-Z][A-Z, ]+:\s/ ||
+                             line =~ /^[A-Z][A-Z, \.-]+:\s/ ||
                              line =~ /^Scene\s+\?\s+-\s+\?/ ||
-                             line =~ /^SONG:/ ||
+                             line =~ START_OF_SONG ||
                              line =~ /^#/
         case line
-        when /^SONG:/
+        when START_OF_SONG
           preserve_breaks = true
-        when /^END OF SONG/
+        when END_OF_SONG
           preserve_breaks = false
         end
         new_content[-1] += ' ' unless new_content[-1] =~ /^$|\s$/
         new_content[-1] += line
+        last_line = line
       end
       
       # Now, insert extra empty lines
@@ -196,18 +211,50 @@ class Fass
           extra_space = false if in_cast
         else
           in_cast = false
-          extra_space = true
+          extra_space = true unless in_song
         end
         new_content << "" if extra_space && new_content.size > 0
         new_content << line
         case line
         when /^#CAST FOR SCENE/
           in_cast = true
-        when /^SONG:/
+        when START_OF_SONG
           extra_space = false
-        when /^END OF SONG/
+          in_song = true
+        when END_OF_SONG
           extra_space = true
-          new_content.pop     # Remove the end of song marker; we don't need it anymore
+          in_song = false
+        end
+      end
+      
+      # Finally, fix songs
+      old_content = new_content
+      new_content = []
+      i = 0
+      while i<old_content.size
+        line = old_content[i]
+        case line
+        when START_OF_SONG
+          # Find lines with stars in them
+          j = i+1
+          while j<old_content.size && old_content[j] !~ END_OF_SONG
+            j += 1
+          end
+          # At this point lines i...j are the song; back up and look for the last "*****"
+          while j>i && old_content[j] !~ /\*{5}/
+            j -= 1
+          end
+          # Now lines (i+1)...j are the song information block
+          song_information = old_content[(i+1)...j].join
+          song_name = song_information[/^[\s\*]*([^\*]+)/,1].strip
+          tune = song_information[/([^\*]+)[\s\*]*$/,1].strip
+          new_content += ["    SONG: #{song_name}", "    (To the tune of: #{tune})"]
+          i = j+1
+        when END_OF_SONG
+          i += 1  # Discard end of song markers; we don't need them anymore
+        else
+          new_content << line
+          i += 1
         end
       end
       
