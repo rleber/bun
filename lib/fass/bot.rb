6b018a3c1d44e385e3a3f68f76d82bd52c0efcca
Richard LeBer <richard.leber@gmail.com>
2012-02-18 15:00:54 -0500

    Refactor cleaning tools into Fass::Script::Cleaner class

--- lib/fass/bot.rb
+++ lib/fass/bot.rb
@@ -10,11 +10,18 @@ class Fass
     SCRIPTS_DIRECTORY = 'scripts'
     
     no_tasks do
-      def fix_script_file(file, ext="script")
+      def fix_script_file(file, *exts)
+        exts = ['.script'] if exts.size == 0
         file = file.dup
         file = "#{SCRIPTS_DIRECTORY}/#{file}" unless file =~ %r{^/}
-        file += ".#{ext}" unless file =~ /\.\w*$/
-        file
+        return file if File.exists?(file)
+        ext.each do |ext|
+          next if ext.nil?  # Special case -- allows you to specify NO substitutions: fix_script_file(file, nil)
+          ext = '.' + ext unless ext =~ /^\./
+          f2 = file + ext
+          return f2 if File.exists?(f2)
+        end
+        file  # If no file exists, return the original file, plus the directory (if any)
       end
     end
     
@@ -33,226 +40,45 @@ class Fass
       script.source_file = file
       puts script.render
     end
+    
+    no_tasks do
+      # Write text to the file, unless file == '-', in which case, write to STDOUT
+      def write_file(file_name, text)
+        if file == '-'
+          puts text
+        else
+          File.open(file_name, 'w') {|f| f.write(text)}
+        end
+      end
+    end
 
-    desc "rtf FILE", "Read a script from an RTF format file"
-    def rtf(file)
-      file = fix_script_file(file, 'rtf')
-      parser = RTF::Parser.new(File.read(file))
-      puts parser.text
+    desc "rtf INPUT_FILE [OUTPUT_FILE]", "Read a script from an RTF format file"
+    long_desc "Output is to <input_file>.raw.txt if none is specified, or to STDOUT if it is \"-\""
+    def rtf(input_file, output_file=nil)
+      input_file = fix_script_file(input_file, '.rtf')
+      output_file = input_file.sub(/\.rtf+$/, '.raw.txt') unless output_file
+      parser = RTF::Parser.new(File.read(input_file))
+      write_file output_file, parser.text
     end
     
-    
-    SCENE_END = /-\s*\bf\s*i\s*n\b\s*-/
-    CAST_OF_CHARACTERS = /cast\s+of\s+characters\s*:\s*$/i
-    CAST_LIST = /^(\s*([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\))+\s*$/i
-    CAST_MEMBER = /([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\)/i
-    
-    desc "clean1 FILE", "Clean an extracted text file (first pass)"
-    def clean1(file)
-      content = File.read(file)
-      content = content.split("\n")
-      title = content.find{|line| line =~ /Page\s+1/}
-      if title
-        title = title[/^.*?\t(.*?)\t/,1].strip if title =~ /\t.*\t/
-        title = title.gsub(/\s+/,' ')
-        puts "#Title: #{title}"
-        puts
-      end
-      new_content = []
-      content.each do |line|
-        line = line.gsub("\t",' ').gsub(/ {2,}/,' ').strip
-        next unless line.size > 0
-        next if line =~ /Page\s+\d+$/
-        next if line =~ /\d+\s+\d+\/\d+\/\s+\d+\s+-\d+:\d+$/
-        next if line =~ /Page\s+\d+.*F\.A\.S\.S/
-        new_content << line
-      end
-      # Find ends of previous scenes
-      scene_ends = [0]
-      new_content.each_with_index do |line, i|
-        if line =~ SCENE_END
-          scene_ends << i+1
-        end
-      end
-      scene_ends.pop # don't need the last one
-      # Find "cast of characters"
-      cast_starts = []
-      new_content.each_with_index do |line, i|
-        if line =~ CAST_OF_CHARACTERS
-          cast_starts << i
-        end
-      end
-      if scene_ends.size == cast_starts.size
-        old_content = new_content
-        new_content = []
-        cast_starts << old_content.size  # Add a sentinel to the end
-        scene_ends.each_with_index do |scene_end, scene_index|
-          scene_name = []
-          line_index = scene_end
-          if line_index == 0
-            # There may be garbage at the beginning of the file; ignore it
-            while old_content[line_index] !~ /[A-Za-z]/
-              line_index += 1
-            end
-            line_index += 1 # And ignore the name of the play
-          end
-          while line_index < cast_starts[scene_index]
-            scene_name << old_content[line_index]
-            line_index += 1
-          end
-          # Because sometime line breaks get missed:
-          scene_name << old_content[line_index].sub(CAST_OF_CHARACTERS,'') if line_index == cast_starts[scene_index]
-          scene_name = scene_name.join(' ').gsub(/\s+/, ' ')
-          new_content << "Scene ? - ? : #{scene_name}\n"
-          new_content << "#CAST FOR SCENE:"
-          line_index += 1
-          cast = []
-          while line_index < cast_starts[scene_index+1] && (old_content[line_index] =~ CAST_LIST || old_content =~ /^\s*$/)
-            new_cast = old_content[line_index].scan(CAST_MEMBER)
-            new_cast.each do |full, nick|
-              cast << [nick.strip, full.strip]
-            end
-            line_index += 1
-          end
-          if cast.size > 0
-            nickname_width = cast.map{|nick, full| nick.size}.max
-            cast.each do |nick, full|
-              new_content << %Q{#  #{"%#{-(nickname_width+1)}s"%(nick+':')}  #{full}}
-            end
-          end
-          new_content << ""
-          while line_index < cast_starts[scene_index+1]
-            break if old_content[line_index] =~ SCENE_END
-            new_content << old_content[line_index]
-            line_index += 1
-          end
-        end
-      else
-        warn "Scene endings (\"fin\") don't match up with casts of characters; unable to isolate scenes"
-        warn "  (There are #{scene_ends.size} scene endings, and #{cast_starts.size} casts of characters.)"
-        warn "  Scene endings: #{scene_ends.inspect}"
-        warn "  Casts of characters: #{cast_starts.inspect}"
-      end
-      puts new_content.join("\n")
+    desc "clean1 INPUT_FILE [OUTPUT_FILE]", "Clean an extracted text file (first pass)"
+    long_desc "Output is to <input_file>.clean1.txt if none is specified, or to STDOUT if it is \"-\""
+    def clean1(input_file, output_file=nil)
+      input_file = fix_script_file(input_file, '.raw.txt', '.txt')
+      output_file = input_file.sub(/(\.raw|\.txt)+$/, '.clean1.txt') unless output_file
+      cleaner = Fass::Script::Cleaner.new(File.read(input_file))
+      write_file output_file, cleaner.clean1
       warn "Please mark the end of each song with \"END OF SONG\" line."
     end
     
-    START_OF_SONG = /^SONG:/
-    END_OF_SONG = /\bEND\s+OF\s+SONG\b/
-    desc "clean2 FILE", "Clean an extracted text file (Second pass)"
-    def clean2(file)
-      content = File.read(file).split("\n")
-      
-      # First, find and mark songs
-      in_song = false
-      new_content = []
-      content.each do |line|
-        if line=~/\*{5}/ .. line=~END_OF_SONG
-          new_content << "SONG:" unless in_song
-          if line =~ END_OF_SONG
-            new_content << line
-            in_song = false
-          else
-            new_content << "    #{line}"
-            in_song = true
-          end
-        else
-          if in_song
-            new_content << "END OF SONG"
-          end
-          in_song = false
-          new_content << line
-        end
-      end
-     
-      # Now, fix line endings and merge lines
-      # TODO Better handling of hypenated words?
-      old_content = new_content
-      new_content = []
-      preserve_breaks = false
-      last_line = ""
-      old_content.each do |line|
-        new_content << "" if preserve_breaks ||
-                             last_line =~ END_OF_SONG || 
-                             new_content.size == 0 ||
-                             line =~ /^.[LS]-\d+(?:\]|$|.\s*\()/ ||
-                             line =~ /^\([A-Z]/ ||
-                             line =~ /^[A-Z][A-Z, \.-]+:\s/ ||
-                             line =~ /^Scene\s+\?\s+-\s+\?/ ||
-                             line =~ START_OF_SONG ||
-                             line =~ /^#/
-        case line
-        when START_OF_SONG
-          preserve_breaks = true
-        when END_OF_SONG
-          preserve_breaks = false
-        end
-        new_content[-1] += ' ' unless new_content[-1] =~ /^$|\s$/
-        new_content[-1] += line
-        last_line = line
-      end
-      
-      # Now, insert extra empty lines
-      old_content = new_content
-      new_content = []
-      extra_space = true
-      in_cast = false
-      in_song = false
-      
-      old_content.each do |line|
-        if line =~ /^#/
-          extra_space = false if in_cast
-        else
-          in_cast = false
-          extra_space = true unless in_song
-        end
-        new_content << "" if extra_space && new_content.size > 0
-        new_content << line
-        case line
-        when /^#CAST FOR SCENE/
-          in_cast = true
-        when START_OF_SONG
-          extra_space = false
-          in_song = true
-        when END_OF_SONG
-          extra_space = true
-          in_song = false
-        end
-      end
-      
-      # Finally, fix songs
-      old_content = new_content
-      new_content = []
-      i = 0
-      while i<old_content.size
-        line = old_content[i]
-        case line
-        when START_OF_SONG
-          # Find lines with stars in them
-          j = i+1
-          while j<old_content.size && old_content[j] !~ END_OF_SONG
-            j += 1
-          end
-          # At this point lines i...j are the song; back up and look for the last "*****"
-          while j>i && old_content[j] !~ /\*{5}/
-            j -= 1
-          end
-          # Now lines (i+1)...j are the song information block
-          song_information = old_content[(i+1)...j].join
-          song_name = song_information[/^[\s\*]*([^\*]+)/,1].strip
-          tune = song_information[/([^\*]+)[\s\*]*$/,1].strip
-          new_content += ["    SONG: #{song_name}", "    (To the tune of: #{tune})"]
-          i = j+1
-        when END_OF_SONG
-          i += 1  # Discard end of song markers; we don't need them anymore
-        else
-          new_content << line
-          i += 1
-        end
-      end
-      
-      # Output the results
-      puts new_content.join("\n")
+    desc "clean2 INPUT_FILE [OUTPUT_FILE]", "Clean an extracted text file (Second pass)"
+    long_desc "Output is to <input_file>.clean.txt if none is specified, or to STDOUT if it is \"-\""
+    def clean2(input_file, output_file=nil)
+      input_file = fix_script_file(input_file, '.clean1.txt', '.txt')
+      output_file = input_file.sub(/(\.raw|\.txt|\.clean|\.clean1)+$/, '.clean.txt') unless output_file
+      input_file = fix_script_file(input_file, 'rtf')
+      cleaner = Fass::Script::Cleaner.new(File.read(input_file))
+      write_file output_file, cleaner.clean2
     end
   end
 end
---
+++ lib/fass/clean.rb
@@ -0,0 +1,232 @@
+class Fass
+  class Script
+    class Cleaner
+      SCENE_END = /-\s*\bf\s*i\s*n\b\s*-/
+      CAST_OF_CHARACTERS = /cast\s+of\s+characters\s*:\s*$/i
+      CAST_LIST = /^(\s*([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\))+\s*$/i
+      CAST_MEMBER = /([a-z][a-z\.\s-]*?)[\*\.\s]+\((.*?)\)/i
+      START_OF_SONG = /^SONG:/
+      END_OF_SONG = /\bEND\s+OF\s+SONG\b/
+      
+      attr_accessor :text
+      
+      def initialize(text)
+        @text = text
+      end
+
+      # First pass cleaning
+      # TODO better elimination of page headings
+      # TODO prompt for song endings; combine with clean2
+      # TODO remember/mark song endings for future runs
+      # TODO allow song endings to be provided as a hash {"SONG_NAME" => <line on which song ends>, ... }
+      # TODO make clean1 repeatable with destructive results
+      
+      def clean1
+        content = text.split("\n")
+        title = content.find{|line| line =~ /Page\s+1/}
+        if title
+          title = title[/^.*?\t(.*?)\t/,1].strip if title =~ /\t.*\t/
+          title = title.gsub(/\s+/,' ')
+          puts "#Title: #{title}"
+          puts
+        end
+        new_content = []
+        content.each do |line|
+          line = line.gsub("\t",' ').gsub(/ {2,}/,' ').strip
+          next unless line.size > 0
+          next if line =~ /Page\s+\d+$/
+          next if line =~ /\d+\s+\d+\/\d+\/\s+\d+\s+-\d+:\d+$/
+          next if line =~ /Page\s+\d+.*F\.A\.S\.S/
+          new_content << line
+        end
+        # Find ends of previous scenes
+        scene_ends = [0]
+        new_content.each_with_index do |line, i|
+          if line =~ SCENE_END
+            scene_ends << i+1
+          end
+        end
+        scene_ends.pop # don't need the last one
+        # Find "cast of characters"
+        cast_starts = []
+        new_content.each_with_index do |line, i|
+          if line =~ CAST_OF_CHARACTERS
+            cast_starts << i
+          end
+        end
+        if scene_ends.size == cast_starts.size
+          old_content = new_content
+          new_content = []
+          cast_starts << old_content.size  # Add a sentinel to the end
+          scene_ends.each_with_index do |scene_end, scene_index|
+            scene_name = []
+            line_index = scene_end
+            if line_index == 0
+              # There may be garbage at the beginning of the file; ignore it
+              while old_content[line_index] !~ /[A-Za-z]/
+                line_index += 1
+              end
+              line_index += 1 # And ignore the name of the play
+            end
+            while line_index < cast_starts[scene_index]
+              scene_name << old_content[line_index]
+              line_index += 1
+            end
+            # Because sometime line breaks get missed:
+            scene_name << old_content[line_index].sub(CAST_OF_CHARACTERS,'') if line_index == cast_starts[scene_index]
+            scene_name = scene_name.join(' ').gsub(/\s+/, ' ')
+            new_content << "Scene ? - ? : #{scene_name}\n"
+            new_content << "#CAST FOR SCENE:"
+            line_index += 1
+            cast = []
+            while line_index < cast_starts[scene_index+1] && (old_content[line_index] =~ CAST_LIST || old_content =~ /^\s*$/)
+              new_cast = old_content[line_index].scan(CAST_MEMBER)
+              new_cast.each do |full, nick|
+                cast << [nick.strip, full.strip]
+              end
+              line_index += 1
+            end
+            if cast.size > 0
+              nickname_width = cast.map{|nick, full| nick.size}.max
+              cast.each do |nick, full|
+                new_content << %Q{#  #{"%#{-(nickname_width+1)}s"%(nick+':')}  #{full}}
+              end
+            end
+            new_content << ""
+            while line_index < cast_starts[scene_index+1]
+              break if old_content[line_index] =~ SCENE_END
+              new_content << old_content[line_index]
+              line_index += 1
+            end
+          end
+        else
+          warn "Scene endings (\"fin\") don't match up with casts of characters; unable to isolate scenes"
+          warn "  (There are #{scene_ends.size} scene endings, and #{cast_starts.size} casts of characters.)"
+          warn "  Scenes end on lines:                #{scene_ends.map{|e| e.to_s}.join(',')}"
+          warn "  Casts of characters start on lines: #{cast_starts.map{|e| e.to_s}.join(',')}"
+        end
+        text = new_content.join("\n")
+      end
+
+      # Second pass cleaning
+      # TODO Better isolation of song names and "to the tune of"s
+      # TODO Better handling of hypenated words when combining lines?
+      # TODO Don't double up line spacing if blank lines already exist
+      # TODO make clean2 repeatable with destructive results
+      
+      def clean2
+        content = text.split("\n")
+  
+        # First, find and mark songs
+        in_song = false
+        new_content = []
+        content.each do |line|
+          if line=~/\*{5}/ .. line=~END_OF_SONG
+            new_content << "SONG:" unless in_song
+            if line =~ END_OF_SONG
+              new_content << line
+              in_song = false
+            else
+              new_content << "    #{line}"
+              in_song = true
+            end
+          else
+            if in_song
+              new_content << "END OF SONG"
+            end
+            in_song = false
+            new_content << line
+          end
+        end
+ 
+        # Now, fix line endings and merge lines
+        old_content = new_content
+        new_content = []
+        preserve_breaks = false
+        last_line = ""
+        old_content.each do |line|
+          new_content << "" if preserve_breaks ||
+                               last_line =~ END_OF_SONG || 
+                               new_content.size == 0 ||
+                               line =~ /^.[LS]-\d+(?:\]|$|.\s*\()/ ||
+                               line =~ /^\([A-Z]/ ||
+                               line =~ /^[A-Z][A-Z, \.-]+:\s/ ||
+                               line =~ /^Scene\s+\?\s+-\s+\?/ ||
+                               line =~ START_OF_SONG ||
+                               line =~ /^#/
+          case line
+          when START_OF_SONG
+            preserve_breaks = true
+          when END_OF_SONG
+            preserve_breaks = false
+          end
+          new_content[-1] += ' ' unless new_content[-1] =~ /^$|\s$/
+          new_content[-1] += line
+          last_line = line
+        end
+  
+        # Now, insert extra empty lines
+        old_content = new_content
+        new_content = []
+        extra_space = true
+        in_cast = false
+        in_song = false
+  
+        old_content.each do |line|
+          if line =~ /^#/
+            extra_space = false if in_cast
+          else
+            in_cast = false
+            extra_space = true unless in_song
+          end
+          new_content << "" if extra_space && new_content.size > 0
+          new_content << line
+          case line
+          when /^#CAST FOR SCENE/
+            in_cast = true
+          when START_OF_SONG
+            extra_space = false
+            in_song = true
+          when END_OF_SONG
+            extra_space = true
+            in_song = false
+          end
+        end
+  
+        # Finally, fix songs
+        old_content = new_content
+        new_content = []
+        i = 0
+        while i<old_content.size
+          line = old_content[i]
+          case line
+          when START_OF_SONG
+            # Find lines with stars in them
+            j = i+1
+            while j<old_content.size && old_content[j] !~ END_OF_SONG
+              j += 1
+            end
+            # At this point lines i...j are the song; back up and look for the last "*****"
+            while j>i && old_content[j] !~ /\*{5}/
+              j -= 1
+            end
+            # Now lines (i+1)...j are the song information block
+            song_information = old_content[(i+1)...j].join
+            song_name = song_information[/^[\s\*]*([^\*]+)/,1].strip
+            tune = song_information[/([^\*]+)[\s\*]*$/,1].strip
+            new_content += ["    SONG: #{song_name}", "    (To the tune of: #{tune})"]
+            i = j+1
+          when END_OF_SONG
+            i += 1  # Discard end of song markers; we don't need them anymore
+          else
+            new_content << line
+            i += 1
+          end
+        end
+  
+        # Save the results
+        text = new_content.join("\n")
+      end
+    end
+  end
+end
