5d5fce5dad3c4616502cefb6cc44a0a7b25c4108
Richard LeBer <richard.leber@gmail.com>
2012-02-25 16:16:54 -0500

    Pretty solid version of MachineWord

--- lib/gecos/machine_word.rb
+++ lib/gecos/machine_word.rb
@@ -17,7 +17,7 @@ class Class
 
   def define_parameter(name, value=nil, &blk)
     value = yield if block_given?
-    const_set name.to_s.upcase, value
+    const_set name.to_s.upcase.sub(/[?!]?$/,''), value
     define_method(name.to_s.downcase) { value }
     singleton_class.instance_eval do
       define_method(name.to_s.downcase) { value }
@@ -56,12 +56,23 @@ end
 
 class MachineWord < GenericNumeric
   
+  class SliceArray < Array
+    def merge
+      self.map{|e| e.string}.join
+    end
+    
+    def values
+      self.map{|e| e.values}
+    end
+  end
+  
   FORMAT_WIDTH_WILDCARD = /<width>/
   define_parameter :formats, {
     :binary=>  "%0#<width>b", 
     :octal=>   "%0#<width>o", 
     :decimal=> "%<width>d",
     :hex=>     "%0#<width>x",
+    :string=>  "%-<width>s"
   }
   
   def self.define_size(word_size)
@@ -84,6 +95,8 @@ class MachineWord < GenericNumeric
     define_collection :n_bit_mask,         n_bit_masks
     define_collection :lower_bit_mask,     lower_bit_masks
     define_collection :upper_bit_mask,     upper_bit_masks
+    
+    define_slice :word, :size=>word_size
   end
   
   def self.bit_mask(from, to)
@@ -125,6 +138,8 @@ class MachineWord < GenericNumeric
     class_name = slice_name.gsub(/(^|_)(.)/) {|match| $2.upcase}
     slice_class = Class.new(GenericNumeric)
     const_set(class_name, slice_class)
+    add_slice(slice_name, options.merge(:class=>slice_class))
+
     slice_class.send(:define_method, :initialize) do |value|
       @value = value
     end
@@ -136,15 +151,19 @@ class MachineWord < GenericNumeric
     shifts = end_bits.map{|end_bit| size - end_bit - 1 }
     masks = (0...per_word).map {|n| slice_mask(n, slice_size, slice_offset) & (clipping_mask << (size - end_bits[n] - 1)) }
     
-    define_parameter "#{slice_name}_size",            slice_size
-    define_parameter "#{slice_name}_offset",          slice_offset
-    define_parameter "#{slice_name}s_per_word",       per_word
-    define_parameter "#{slice_name}_clipping_mask",   clipping_mask
+    define_parameter "#{slice_name}_size",             slice_size
+    define_parameter "#{slice_name}_offset",           slice_offset
+    define_parameter "#{slice_name}s_per_word",        per_word
+    define_parameter "#{slice_name}_significant_bits", bits
+    define_parameter "#{slice_name}_string?",          !!options[:string]
+    define_parameter "#{slice_name}_clipping_mask",    clipping_mask
     
-    slice_class.define_parameter "size",              slice_size
-    slice_class.define_parameter "offset",            slice_offset
-    slice_class.define_parameter "per_word",          per_word
-    slice_class.define_parameter "clipping_mask",     clipping_mask
+    slice_class.define_parameter  "size",              slice_size
+    slice_class.define_parameter  "offset",            slice_offset
+    slice_class.define_parameter  "per_word",          per_word
+    slice_class.define_parameter  "significant_bits",  bits
+    slice_class.define_parameter  "string?",           !!options[:string]
+    slice_class.define_parameter  "clipping_mask",     clipping_mask
     
     define_collection "#{slice_name}_end_bit",   end_bits
     define_collection "#{slice_name}_start_bit", start_bits
@@ -156,14 +175,21 @@ class MachineWord < GenericNumeric
     slice_class.define_collection "shift",       shifts
     slice_class.define_collection "mask",        masks
     
-    slice_formats = formats.dup
-    slice_formats.each do |format, definition|
+    slice_formats = {}
+    # TODO Special case for single bits (binary, octal, decimal, and hex representations are all the same)
+    formats.each do |format, definition|
+      next if format==:string && !options[:string]
+      sample_values = [n_bit_masks[bits], n_bit_masks[bits-1]||0, 0]
+      if format!=:string && bits == 1 # Special case for single bits (binary, octal, decimal, and hex representations are all the same)
+        definition = definition.gsub(/%.*(?:#{FORMAT_WIDTH_WILDCARD})?.*?([a-z])/, '%\1')
+      end
       sample_definition = definition.gsub(FORMAT_WIDTH_WILDCARD, '')
-      sample_text = sample_definition % [n_bit_masks[bits]]
-      width = sample_text.size
+      sample_values = sample_values.map{|v| v.chr } if format == :string && options[:string]
+      sample_texts = sample_values.map{|v| sample_definition % [v] }
+      width = sample_texts.map{|t| t.size}.max
       definition = definition.gsub(FORMAT_WIDTH_WILDCARD, width.to_s)
-      slice_formats[format] = definition
-      slice_class.define_parameter "#{format}_format_string", definition
+      slice_formats[format] = {:name=>format, :string=>definition, :max_width=>width}
+      slice_class.define_parameter "#{format}_format_string", definition[:string]
       slice_class.send(:define_method, format.to_s) do
         definition % [self.value]
       end
@@ -171,17 +197,28 @@ class MachineWord < GenericNumeric
     slice_formats[:default] = slice_formats[default_format]
     
     if options[:string]
-      slice_formats[:inspect] = slice_formats[:octal] + ' %c'
+      slice_formats[:inspect] = {
+        :name=>:inspect, :string=>slice_formats[:octal][:string] + '::' + slice_formats[:string][:string], 
+        :max_width=>slice_formats[:octal][:max_width] + 2 + slice_formats[:string][:max_width]
+      }
     else
       slice_formats[:inspect] = slice_formats[:default]
     end
-    slice_class.define_collection "format_string", slice_formats
+    format_strings = slice_formats.inject({}){|hsh, kv| key, value = kv; hsh[key] = value[:string]; hsh}
+    format_widths  = slice_formats.inject({}){|hsh, kv| key, value = kv; hsh[key] = value[:max_width]; hsh}
+    slice_class.define_collection "format_definition", slice_formats
+    slice_class.define_collection "format_string", format_strings
+    slice_class.define_collection "format_width",  format_widths
+    slice_class.define_collection "format_type",   slice_formats.keys.sort_by{|f| f.to_s}
     
     slice_class.send(:define_method, :format) do |*args|
       format = args[0] || :default
-      format = slice_formats[format] if format.is_a?(Symbol)
-      values = [self.value] * (format.gsub(/[^%]/,'').size)
-      format % values
+      format_string = format
+      format_string = slice_formats[format][:string] if format.is_a?(Symbol)
+      v = self.value
+      v = v.chr if format==:string && options[:string]
+      values = [v] * (format_string.gsub(/[^%]/,'').size)
+      format_string % values
     end
     
     slice_class.send(:define_method, :inspect) do
@@ -197,12 +234,17 @@ class MachineWord < GenericNumeric
       slice_class.new(self.send(unshifted_method_name, n) >> shifts[n])
     end
     define_method "#{slice_name}s" do
-      (0...per_word).map {|n| self.send(slice_name, n) }
+      ary = SliceArray.new
+      (0...per_word).each {|n| ary << self.send(slice_name, n) }
+      ary
     end
 
     if options[:string]
-      slice_class.send(:define_method, :string) do |n|
+      slice_class.send(:define_method, :string) do
         self.chr
+      end
+      slice_class.send(:define_method, :string_inspect) do
+        self.string.inspect
       end
       define_method "#{slice_name}_string" do |n|
         (0...per_word).map {|n| self.send(slice_name, n).string }
@@ -237,5 +279,17 @@ class MachineWord < GenericNumeric
   def slice(n, size, offset=0)
     start = (n-1)*size + offset
     bits(start, start+size-1)
+  end
+  
+  def self.slices
+    @slices ||=[]
+  end
+  
+  def self.add_slice(name, definition)
+    self.slices << definition.merge(:name=>name.to_sym)
+  end
+  
+  def self.slice_names
+    self.slices.map{|slice| slice[:name]}
   end
 end
