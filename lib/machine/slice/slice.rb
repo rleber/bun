494851a89ba563ff331bdd88b4a0114ef63f3aaa
Richard LeBer <richard.leber@gmail.com>
2012-03-05 15:30:54 -0500

    Fixed problems with signed formats

--- lib/machine/slice/slice.rb
+++ lib/machine/slice/slice.rb
@@ -60,79 +60,69 @@ module Machine
             single_bit_mask(width-sign_bit-1)
           end
           
+          def encoded_sign(val)
+            (val & sign_mask) >> (width-sign_bit-1)
+          end
+
           def sign(val)
-            val & sign_mask
+            val < 0 ? 1 : 0
+          end
+          
+          def ones_complement(value)
+            clip( ~value )
+          end
+          
+          def twos_complement(value)
+            clip (~value + 1)
+          end
+          
+          def encode(value)
+            value < 0 ? complement(value.abs) : value
+          end
+          
+          def decode(value)
+            encoded_sign(value)==0 ? value : -complement(value)
           end
         end
         
         def initialize(val, options={})
-          warn "In Signed::Base.new(#{val.inspect} (class #{val.class}), #{options.inspect})"
-          if val<0
-            val = self.class.complement(-val)
+          if val>=0
+            val = self.class.decode(val)
           end
           super(val)
-          @ignore_sign = options[:ignore_sign]
+          # @ignore_sign = options[:ignore_sign]
         end
 
-        def value
-          _signed
-        end
-    
         def sign
-          (@ignore_sign ? 0 : self.class.sign(internal_value)) >> (self.class.width-self.class.sign_bit-1)
+          self.class.sign(internal_value)
         end
 
-        def complement
-          self.class.new(_complement, :ignore_sign=>@ignore_sign)
+        def ones_complement
+          self.class.ones_complement(internal_value)
         end
 
-        def _complement
-          self.class.complement(internal_value)
+        def twos_complement
+          self.class.twos_complement(internal_value)
         end
-        private :_complement
       
         def signed
-          self.class.new(_signed, :ignore_sign=>false)
+          value
         end
-    
-        def _signed
-          sign==0 ? _unsigned : -_abs
-        end
-        private :_signed
-        
+
         def unsigned
-          self.class.new(_unsigned, :ignore_sign=>true)
+          self.class.encode(internal_value)
         end
-        
-        def _unsigned
-          internal_value
-        end
-        private :_unsigned
-      
-        def abs
-          self.class.new(_abs, :ignore_sign=>@ignore_sign)
-        end
-        
-        def _abs
-          sign==0 ? _unsigned : _complement
-        end
-        private :_abs
       end
       
       class TwosComplement < Slice::Signed::Base
         class << self
           def complement(value)
-            clip( ~value + 1)
+            twos_complement(value)
           end
           
           def sign_type
             :twos_complement
           end
-        end
-        
-        def initialize(val, options={})
-          warn "In TwosComplement.new(#{val.inspect} (class #{val.class}), #{options.inspect})"
-          super
         end
       end
       
@@ -146,12 +136,6 @@ module Machine
           def sign_type
             :ones_complement
           end
-        end
-        
-        def initialize(value, options={})
-          warn "In OnesComplement.new(#{value.inspect} (class #{value.class}), #{options.inspect})"
-          warn %Q{caller:\n#{caller.map{|s| '  ' + s}.join("\n")}}
-          super
         end
       end
     end
