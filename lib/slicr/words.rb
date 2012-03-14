c519553f2bbf9b0c7a784586be302be2c6b1b8b8
Richard LeBer <richard.leber@gmail.com>
2012-03-14 11:57:04 -0400

    Remove extra versions of Slicr::Words#import

--- lib/slicr/words.rb
+++ lib/slicr/words.rb
@@ -69,26 +69,8 @@ module Slicr
       end
       
       # Import a Ruby string (of 8-bit characters) into words
-      # def import(content)
-      #   width = constituent_class.width
-      #   case constituent_class.width % 8
-      #   when 0
-      #   when 4
-      #   else
-      #     import_by_bits(content)
-      #   end
-      # end
+      # See faster_import branch for a variety of different implementations
       def import(content)
-        import_by_bytes_with_unpack(content) # Fastest so far
-        # import_by_chunks(content, 8, 'C*') # Very slightly slower than import_by_bytes_with_unpack
-        # import_by_chunks(content, 32, 'N*') # This is slightly slower than importing by C*
-        # import_by_hex_nibbles(content, 4, 'H*')
-        # import_by_hex_words(content)
-      end
-      
-      # This works and is about 12-15% faster than import_by_bytes;
-      # It is very slightly faster than import_by_chunks(content, 8, 'C*')
-      def import_by_bytes_with_unpack(content)
         words = []
         accumulator = 0
         bits = 0
@@ -116,120 +98,7 @@ module Slicr
         end
         new(words)
       end
-      
-      # This works and is about 12-15% faster than import_by_bytes
-      def import_by_chunks(content, bits_per_chunk, unpack_mask)
-        words = []
-        accumulator = 0
-        bits = 0
-        width = constituent_class.width
-        bytes_per_chunk = (bits_per_chunk+7).div(8)
-        padding_size = (bytes_per_chunk - content.size % bytes_per_chunk) % bytes_per_chunk
-        content = content + "\0"*padding_size if padding_size > 0
-        chunks = content.unpack(unpack_mask)
-        chunks.each do |chunk|
-          accumulator <<= bits_per_chunk # Would checking for zero be faster?
-          accumulator |= chunk
-          bits += bits_per_chunk
-          while bits >= width
-            words << (accumulator >> (bits - width))
-            accumulator &= (2**(bits-width)-1) # Mask off upper bits
-            bits -= width 
-          end
-          # puts "bits=#{bits}, accumulator=#{accumulator} words=#{words.inspect}"
-        end
-        if bits > padding_size*8
-          words << (accumulator << (width - bits))
-        end
-        p words.size
-        $count ||=0
-        $count += 1
-        if $count == 1
-          p words
-          exit
-        end
-        new(words)
-      end
-      
-      # This is very slightly slower than import_by_bytes_with_unpack
-      def import_by_hex_words(content)
-        width = constituent_class.width
-        hex = content.unpack('H*').first
-        word_nibbles = width.div(4)
-        word_pattern = /.{1,#{word_nibbles}}/
-        words = hex.scan(word_pattern).map do |chunk|
-          chunk = (chunk + '0'*word_nibbles)[0,word_nibbles] if chunk.size < word_nibbles
-          words << Integer('0x' + chunk)
-        end
-        new(words)
-      end
 
-      def import_by_bytes(content)
-        words = []
-        accumulator = 0
-        bits = 0
-        width = constituent_class.width
-        content.each_byte do |ch|
-          accumulator <<= 8
-          accumulator |= ch
-          bits += 8
-          while bits >= width
-            words << (accumulator >> (bits - width))
-            accumulator &= (2**(bits-width)-1) # Mask off upper bits
-            bits -= width 
-          end
-        end
-        if bits > 0
-          words << (accumulator << (width - bits))
-        end
-        new(words)
-      end
-      
-      # This is slower than import_by_bytes
-      def import_by_hex_nibbles(content, bits_per_chunk, unpack_mask)
-        words = []
-        accumulator = 0
-        bits = 0
-        width = constituent_class.width
-        bytes_per_chunk = (bits_per_chunk+7).div(8)
-        padding_size = (bytes_per_chunk - content.size % bytes_per_chunk) % bytes_per_chunk
-        content = content + "\0"*padding_size if padding_size > 0
-        chunks = content.unpack(unpack_mask)
-        chunks.first.scan(/./).each do |chunk|
-          chunk = Integer('0x' + chunk)
-          accumulator <<= bits_per_chunk # Would checking for zero be faster?
-          accumulator |= chunk
-          bits += bits_per_chunk
-          while bits >= width
-            words << (accumulator >> (bits - width))
-            accumulator &= (2**(bits-width)-1) # Mask off upper bits
-            bits -= width 
-          end
-          # puts "bits=#{bits}, accumulator=#{accumulator} words=#{words.inspect}"
-        end
-        if bits > padding_size*8
-          words << (accumulator << (width - bits))
-        end
-        # p words.size
-        # $count ||=0
-        # $count += 1
-        # if $count == 1
-        #   p words
-        #   exit
-        # end
-        new(words)
-      end
-      
-      # This is slower than import_by_bytes
-      def import_bits_by_unpack(content)
-        width = constituent_class.width
-        bits = content.unpack("B#{8*content.size}").first
-        word_bits = (bits + '0'*width).scan(/.{#{width}}/) # Add the zeros to ensure a complete last word
-        word_bits.pop # There will always be extra zeros at the end
-        words = word_bits.map{|word| Integer('0b' + word)}
-        new(words)
-      end
-      
       def read(file, options={})
         if file.is_a?(String)
           abort "File #{file} does not exist" unless ::File.exists?(file)
