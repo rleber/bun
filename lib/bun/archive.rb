49e486de347723f45b4eb5c9c6d88994f476ab02
Richard LeBer <richard.leber@gmail.com>
2012-03-15 19:20:17 -0400

    Debugged check_original; spiffed up format (a lot)

--- lib/bun/archive.rb
+++ lib/bun/archive.rb
@@ -226,6 +226,7 @@ class Bun
       ::File.open(index_file, 'w') {|f| f.write index.to_yaml }
     end
     
+    # TODO Build an OpenStruct type object for descriptor
     def descriptor(name, options={})
       if !exists?(name)
         nil
