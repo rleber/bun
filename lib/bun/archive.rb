2a9fcffa5f32534828435256e581570ddc9433d4
Richard LeBer <richard.leber@gmail.com>
2012-03-22 08:35:13 -0400

    Refactor cp logic into Archive

--- lib/bun/archive.rb
+++ lib/bun/archive.rb
@@ -297,5 +297,30 @@ module Bun
     def exists?(name)
       File.exists?(expanded_tape_path(name))
     end
+    
+    def cp(tape, dest=nil, options={})
+      to_stdout = dest.nil? || dest == '-'
+      index = !options[:bare] && !to_stdout
+      unless to_stdout
+        dest = '.' if dest == ''
+        dest = File.join(dest, File.basename(tape)) if File.directory?(dest)
+      end
+
+      open(tape) do |f|
+        Shell.new(:quiet=>true).write dest, f.read, :mode=>'w:ascii-8bit'
+      end
+
+      if index
+        # Copy index entry, too
+        to_dir = File.dirname(dest)
+        to_archive = Archive.new(:location=>to_dir, :directory=>'')
+        descriptor = self.descriptor(tape)
+        descriptor.original_tape_name = tape unless descriptor.original_tape_name
+        descriptor.original_tape_path = expanded_tape_path(tape) unless descriptor.original_tape_path
+        descriptor.tape_name = File.basename(dest)
+        descriptor.tape_path = File.expand_path(dest)
+        to_archive.update_index(:descriptor=>descriptor)
+      end
+    end
   end
 end
--- lib/bun/bots/main/cp_task.rb
+++ lib/bun/bots/main/cp_task.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # -*- encoding: us-ascii -*-
 
-# TODO Move core capability to Archive; refactor
 # TODO Copy more than one file
 # TODO Copy by pattern
 # TODO Create mv command
@@ -12,26 +11,5 @@ option 'bare',    :aliases=>'-b', :type=>'boolean', :desc=>'Copy file, but not i
 def cp(tape, dest = nil)
   directory = options[:archive] || Archive.location
   from_archive = Archive.new(:location=>directory)
-  to_stdout = dest.nil? || dest == '-'
-  index = !options[:bare] && !to_stdout
-  unless to_stdout
-    dest = '.' if dest == ''
-    dest = File.join(dest, File.basename(tape)) if File.directory?(dest)
-  end
-
-  from_archive.open(tape) do |f|
-    Shell.new(:quiet=>true).write dest, f.read, :mode=>'w:ascii-8bit'
-  end
-
-  if index
-    # Copy index entry, too
-    to_dir = File.dirname(dest)
-    to_archive = Archive.new(:location=>to_dir, :directory=>'')
-    descriptor = from_archive.descriptor(tape)
-    descriptor.original_tape_name = tape unless descriptor.original_tape_name
-    descriptor.original_tape_path = from_archive.expanded_tape_path(tape) unless descriptor.original_tape_path
-    descriptor.tape_name = File.basename(dest)
-    descriptor.tape_path = File.expand_path(dest)
-    to_archive.update_index(:descriptor=>descriptor)
-  end
+  from_archive.cp(tape, dest, options)
 end
