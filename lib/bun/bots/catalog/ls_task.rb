5d4ce3a0eb76bc8bf13db9e1f61eba8e465ddcd3
Richard LeBer <richard.leber@gmail.com>
2012-03-29 22:32:45 -0400

    Add column headings to git catalog ls

--- lib/bun/bots/catalog/ls_task.rb
+++ lib/bun/bots/catalog/ls_task.rb
@@ -4,8 +4,8 @@
 desc "ls", "List the catalog file for the archive"
 option 'archive', :aliases=>'-a', :type=>'string', :desc=>'Archive location'
 def ls
-  archive = Archive.new(options[:archive])
-  # TODO Use Array.justify_rows
+  archive = Archive.new(:location=>options[:archive])
+  puts "Location    Update      File/Directory"
   archive.catalog.each do |spec|
     puts "#{spec[:location]}  #{spec[:date].strftime('%Y/%d/%m')}  #{spec[:file]}"
   end
