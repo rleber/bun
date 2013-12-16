7240a474ffb1e28c9c330fb1f9239d96598aa15d
Richard LeBer <richard.leber@gmail.com>
2013-12-15 20:10:40 -0500

    Refactor bun config ls

--- lib/bun/bots/config/ls_task.rb
+++ lib/bun/bots/config/ls_task.rb
@@ -13,10 +13,9 @@ def ls(pat=nil)
   pat = Bun.convert_glob(pat)
   selected_tasks = tasks.select {|task| task =~ pat }
   if selected_tasks.size > 0
-    task_name_size = selected_tasks.map{|task| task.size}.max
-    selected_tasks.each do |task|
-      value = (options[:expand] && task =~ /_path$/) ? config.expanded_setting(task) : config.setting[task]
-      puts "#{task.ljust(task_name_size)}  #{value}"
+    table = [["Config","Setting"]] + selected_tasks.map do |task|
+      [task, (options[:expand] && task =~ /_path$/) ? config.expanded_setting(task) : config.setting[task]]
     end
+    puts table.justify_rows.map{|row| row.join('  ')}.join("\n")
   end
 end
