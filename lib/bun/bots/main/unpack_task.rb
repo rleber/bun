5f03e1d311a51ced8578145438f01c2c4016c167
Richard LeBer <richard.leber@gmail.com>
2013-12-28 12:02:04 -0500

    Remove bun unpack

--- lib/bun/bots/main/unpack_task.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# -*- encoding: us-ascii -*-
-
-desc "unpack FILE [TO]", "Read in a packed Bun file and translate to a flat YAML format"
-def unpack(file, to=nil)
-  File.unpack(file, to)
-end
