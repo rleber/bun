a66363c6486f26f549e1ace205d63073c02e5232
Richard LeBer <richard.leber@gmail.com>
2014-01-02 12:55:59 -0500

    bun unpack and bun decode now promote files

--- lib/bun/bots/main/bake_task.rb
+++ lib/bun/bots/main/bake_task.rb
@@ -2,7 +2,8 @@
 # -*- encoding: us-ascii -*-
 
 desc "bake FILE [TO]", "Output the ASCII content for the files"
+option 'asis',  :aliases=>'-a', :type=>'boolean', :desc=>"Do not attempt to decode file first"
 option "tape", :aliases=>'-t', :type=>'string',  :desc=>"Supply tape name (use with input from STDIN)"
 def bake(from, to='-')
-  File::Decoded.bake(from, to, options.merge(promote: true))
+  File::Decoded.bake(from, to, options.merge(promote: !options[:asis]))
 end
