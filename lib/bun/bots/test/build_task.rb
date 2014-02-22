c060dae17f0ae6e380f350e4c8c1980c6e221f15
Richard LeBer <richard.leber@gmail.com>
2014-02-22 11:07:45 -0500

    Restructure tests for compress with conflicts NOT to depend directly on decoded FASS and Watbun archives

--- lib/bun/bots/test/build_task.rb
+++ lib/bun/bots/test/build_task.rb
@@ -200,6 +200,8 @@ def build
   build_compress_test_directory "data/test/archive/compress_init", quiet: options[:quiet]
   build_compress_test_directory "data/test/archive/same", quiet: options[:quiet]
 
-  # Build compress conflict test -- # This depends on watbun files!
-  _exec "build_compress_conflict_test"
+  # Build compress conflict test
+  build_directory("data/test/archive/compress_conflict_init", quiet: options[:quiet]) do |dir|
+    _exec "cp -rf data/test_init/compress_conflict #{at.safe}"
+  end
 end
